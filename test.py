eab52cf4525b30cbe750de589958e275db9cd75f
Elon <elonmusk@gmail.com>
2022-11-12 14:43:26 -0600

    a

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import socketio
 
 sio = socketio.Client()
-sio.connect("http://cheesehacks-backend.herokuapp.com/")
+sio.connect("http://cheesehacks-backend.herokuapp.com:5000/")
 
 print(sio.sid)
 sio.disconnect()
